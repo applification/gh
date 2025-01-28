512d0d5b3b172eeaf566b9794669c0acad480b8e
Dave Hudson <dave@applification.net>
2025-01-28 11:40:30 +0000

    add about route and corresponding component for new page

---
+++ app/routes/about.tsx
@@ -0,0 +1,9 @@
+import type { Route } from "./+types/about";
+
+export function meta({}: Route.MetaArgs) {
+  return [{ title: "New React Router App" }, { name: "description", content: "Welcome to React Router!" }];
+}
+
+export default function About() {
+  return <h2>A new page</h2>;
+}
